109d73d59c762093e3534f1725a090dc0289028d
Rasel Mahmud <raselm606@gmail.com>
2025-12-06 08:31:26 +0600

    class na

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,21 @@ const nextConfig = {
   /* config options here */
   
   reactCompiler: true,
-  images: {
-    domains: [
-      'beatai.io', 
-      'beatbind.io', 
-      'beatbuy.io', 
-      'openai.com', 
-      'gemini.google.com', 
-      'www.google.com', 
-      'qwenlm.github.io',
-      'grok.com',
-      'claude.ai',
-      'www.deepseek.com',
-      'chatgpt.com',
-    ],
+  images: { 
+
+    remotePatterns: [
+    { protocol: "https", hostname: "beatai.io" },
+    { protocol: "https", hostname: "beatbind.io" },
+    { protocol: "https", hostname: "beatbuy.io" },
+    { protocol: "https", hostname: "openai.com" },
+    { protocol: "https", hostname: "gemini.google.com" },
+    { protocol: "https", hostname: "www.google.com" },
+    { protocol: "https", hostname: "qwenlm.github.io" },
+    { protocol: "https", hostname: "grok.com" },
+    { protocol: "https", hostname: "claude.ai" },
+    { protocol: "https", hostname: "www.deepseek.com" },
+    { protocol: "https", hostname: "chatgpt.com" },
+  ],
   },
 };
 
